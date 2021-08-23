7f3fddd79f67a4df95e61ded4d6319ce31a6e9cd
Vinícius Moura Spindola <viniciusmspindola@gmail.com>
2021-08-23 12:43:30 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var link = document.getElementById('link')
+var video = document.getElementById('video')
+var modBarra = document.getElementById('modBarra')
+var playId = document.getElementById('play')
+var pauseId = document.getElementById('pause')
+var t1 = document.getElementById('t1')
+var t2 = document.getElementById('t2')
+var t3 = document.getElementById('t3')
+function acao(){
+	link.value = link.value.replace('youtu.be', "www.youtube.com/embed");
+	video.innerHTML = '<iframe width="100%" height="100%" src="'+ link.value + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+}
+var pause = true
+function play(){
+	pause = false
+	playId.className = 'none'
+	pauseId.classList.remove("none");
+}
+function pausar(){
+	pause = true
+	pauseId.className = 'none'
+	playId.classList.remove("none");
+}
+var tempoPorcentagem = 0
+var cronoPorcentagem = setInterval(function(){
+	if(pause == false){
+		tempoPorcentagem++
+		modBarra.innerHTML = '<div id="barra" style="background: linear-gradient(to right, #82FA58 '+ tempoPorcentagem + '%, gray ' + /*(100 - tempoPorcentagem) + */'0%);"></div>'
+	}
+	if(tempoPorcentagem == 15){
+	t1.classList.remove('opacidade')
+	}
+	if(tempoPorcentagem == 50){
+		t2.classList.remove('opacidade')
+	}
+	if(tempoPorcentagem == 100){
+		t3.classList.remove('opacidade')		
+	}
+	if (tempoPorcentagem == 101) {
+		alert('Parabéns, você completou as duas horas')
+	}
+}, 72000)
